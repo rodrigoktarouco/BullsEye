4db654cf932c69142111517a6827aeac6a4bb018
Rodrigo Kroef tarouco <55713305+rodrigoktarouco@users.noreply.github.com>
2022-06-11 17:41:28 -0300

    First Commit

---
+++ BullsEye/BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Rodrigo Tarouco on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
