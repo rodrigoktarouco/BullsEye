3553ea39f40672c02e6bde94fb830822737200df
Rodrigo Kroef tarouco <rodrigoktarouco@gmail.com>
2022-06-24 11:48:22 -0300

    More adjusments backgroundColor, ButtonColor, and another modifiers 	modified:   BullsEye/BullsEye/App/Assets.xcassets/AccentColor.colorset/Contents.json

--- BullsEye/BullsEye/Views/ContentView.swift
+++ BullsEye/BullsEye/Views/ContentView.swift
@@ -14,43 +14,50 @@ struct ContentView: View {
     @State private var game = Game()
     
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯\nput the bulls eye as close as you can".uppercased())
-                .kerning(2.0)
-                .bold()
-                .multilineTextAlignment(.center)
-                .lineSpacing(4)
-                .font(.footnote)
-                .padding()
-            Text("\(game.target)")
-                .kerning(-1.0)
-                .fontWeight(.black)
-                .font(.largeTitle)
-            HStack {
-                Text("1")
-                    .font(.headline)
-                Slider(value: $sliderValue, in: 1.0...100.0).padding(.horizontal)
-                Text("100")
-                    .font(.headline)
-            }.padding(.horizontal)
-            Button {
-                withAnimation {
-                    alertIsVisible = true
-                }
-            } label: {
-                Text("Hit me".uppercased())
+        ZStack {
+            Color("BackgroundColor").edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("🎯🎯🎯\nput the bulls eye as close as you can".uppercased())
+                    .kerning(2.0)
                     .bold()
-                    .font(.title3)
-            }
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(21)
-            .alert("Hello there!", isPresented: $alertIsVisible) {
-                Button("Awesome!") {}
-            } message: {
-                let roundedValue = Int(sliderValue.rounded())
-                Text("The slider's value is \(roundedValue).\n" + "You scored \(game.returnPoints(sliderValue: roundedValue)) points.")
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4)
+                    .font(.footnote)
+                    .padding()
+                Text("\(game.target)")
+                    .kerning(-1.0)
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                HStack {
+                    Text("1")
+                        .font(.headline)
+                    Slider(value: $sliderValue, in: 1.0...100.0).padding(.horizontal)
+                    Text("100")
+                        .font(.headline)
+                }.padding(.horizontal)
+                Button {
+                    withAnimation {
+                        alertIsVisible = true
+                    }
+                } label: {
+                    Text("Hit me".uppercased())
+                        .bold()
+                        .font(.title3)
+                }
+                .padding()
+                .background(
+                    ZStack {
+                        Color("ButtonBackgroundColor")
+                        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(30), Color.clear]), startPoint: .top, endPoint: .bottom)
+                    })
+                .foregroundColor(.white)
+                .cornerRadius(21)
+                .alert("Hello there!", isPresented: $alertIsVisible) {
+                    Button("Awesome!") {}
+                } message: {
+                    let roundedValue = Int(sliderValue.rounded())
+                    Text("The slider's value is \(roundedValue).\n" + "You scored \(game.returnPoints(sliderValue: roundedValue)) points.")
+                }
             }
         }
     }
